bdefbc54faca5d1641a9d243eec9f9701371dab7
loeken <loeken@internetz.me>
2023-11-18 15:07:58 +0100

    testing

--- proxmox/client.go
+++ proxmox/client.go
@@ -1,6 +1,6 @@
 package proxmox
 
-// inspired by https://github.com/Telmate/vagrant-proxmox/blob/master/lib/vagrant-proxmox/proxmox/connection.rb
+// inspired by https://github.com/loeken/vagrant-proxmox/blob/master/lib/vagrant-proxmox/proxmox/connection.rb
 
 import (
 	"bytes"
